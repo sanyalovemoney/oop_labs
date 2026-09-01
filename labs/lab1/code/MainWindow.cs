39bf58bf4db5952007fbfd902bab73df40850ff8
sanyalovemoney <cashamashuta@gmail.com>
2026-09-01 14:25:00 +0300

    Lab1: create WinForms project skeleton — csproj, entry point, empty MainWindow

---
+++ labs/lab1/code/MainWindow.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            this.Text = "Лабораторна робота №1 (C#)";
+            this.Size = new Size(600, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
