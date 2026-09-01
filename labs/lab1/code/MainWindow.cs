3f4b72367456a6992b3f34e5412427b71cbd0557
sanyalovemoney <cashamashuta@gmail.com>
2026-09-01 17:55:00 +0300

    Lab1: add main window with menu (Роботи/Допомога/Файл) and result label

--- labs/lab1/code/MainWindow.cs
+++ labs/lab1/code/MainWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -5,11 +6,62 @@ namespace Lab1
 {
     public class MainWindow : Form
     {
+        private MenuStrip menuStrip;
+        private Label lblDisplayText;
+
         public MainWindow()
         {
             this.Text = "Лабораторна робота №1 (C#)";
             this.Size = new Size(600, 400);
             this.StartPosition = FormStartPosition.CenterScreen;
+
+            menuStrip = new MenuStrip();
+            var menuWorks = new ToolStripMenuItem("Роботи");
+            var menuHelp = new ToolStripMenuItem("Допомога");
+            var menuFile = new ToolStripMenuItem("Файл");
+
+            var itemWork1 = new ToolStripMenuItem("Робота 1", null, OnWork1Clicked);
+            var itemWork2 = new ToolStripMenuItem("Робота 2", null, OnWork2Clicked);
+            menuWorks.DropDownItems.Add(itemWork1);
+            menuWorks.DropDownItems.Add(itemWork2);
+
+            var itemAbout = new ToolStripMenuItem("Про програму...", null, OnAboutClicked);
+            menuHelp.DropDownItems.Add(itemAbout);
+
+            var itemExit = new ToolStripMenuItem("Вихід", null, (s, e) => Application.Exit());
+            menuFile.DropDownItems.Add(itemExit);
+
+            menuStrip.Items.Add(menuFile);
+            menuStrip.Items.Add(menuWorks);
+            menuStrip.Items.Add(menuHelp);
+
+            this.MainMenuStrip = menuStrip;
+            this.Controls.Add(menuStrip);
+
+            lblDisplayText = new Label
+            {
+                Text = "Будь ласка, оберіть роботу в меню",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill,
+                Font = new Font("Segoe UI", 12, FontStyle.Regular)
+            };
+            this.Controls.Add(lblDisplayText);
+            lblDisplayText.BringToFront();
+        }
+
+        private void OnWork1Clicked(object? sender, EventArgs e)
+        {
+            lblDisplayText.Text = "Робота 1 ще не реалізована.";
+        }
+
+        private void OnWork2Clicked(object? sender, EventArgs e)
+        {
+            lblDisplayText.Text = "Робота 2 ще не реалізована.";
+        }
+
+        private void OnAboutClicked(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Лабораторна робота №1\nСтудент: Мащута Олександр", "Про програму", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
