46fa11c41e1e79f5a074938429334a5ee579df30
sanyalovemoney <cashamashuta@gmail.com>
2026-09-06 16:20:00 +0300

    Lab3: add main window with Objects menu and mouse drawing (from Lab2)

--- labs/lab3/code/MainWindow.cs
+++ labs/lab3/code/MainWindow.cs
@@ -24,6 +24,103 @@ namespace Lab3
             this.Text = "Graphic Object Editor - Lab 3";
             this.Size = new Size(800, 600);
             this.DoubleBuffered = true;
+
+            InitializeMenu();
+        }
+
+        private void InitializeMenu()
+        {
+            _menuStrip = new MenuStrip();
+            var objectsMenu = new ToolStripMenuItem("Objects");
+
+            objectsMenu.DropDownItems.Add("Point", null, (s, e) => { _currentType = ShapeType.Point; UpdateTitle(); });
+            objectsMenu.DropDownItems.Add("Line", null, (s, e) => { _currentType = ShapeType.Line; UpdateTitle(); });
+            objectsMenu.DropDownItems.Add("Rectangle", null, (s, e) => { _currentType = ShapeType.Rectangle; UpdateTitle(); });
+            objectsMenu.DropDownItems.Add("Ellipse", null, (s, e) => { _currentType = ShapeType.Ellipse; UpdateTitle(); });
+
+            _menuStrip.Items.Add(objectsMenu);
+            this.MainMenuStrip = _menuStrip;
+            this.Controls.Add(_menuStrip);
+
+            UpdateTitle();
+        }
+
+        private void UpdateTitle()
+        {
+            this.Text = $"Graphic Object Editor - Lab 3 [{_currentType}]";
+        }
+
+        private static Shape CreateShape(ShapeType type, Point start, Point end) => type switch
+        {
+            ShapeType.Point => new PointShape(start.X, start.Y, end.X, end.Y),
+            ShapeType.Line => new LineShape(start.X, start.Y, end.X, end.Y),
+            ShapeType.Rectangle => new RectShape(start.X, start.Y, end.X, end.Y),
+            ShapeType.Ellipse => new EllipseShape(start.X, start.Y, end.X, end.Y),
+            _ => throw new ArgumentOutOfRangeException(nameof(type))
+        };
+
+        private static Brush GetFillBrush(Shape shape) => shape switch
+        {
+            RectShape => Brushes.Orange,
+            EllipseShape => Brushes.White,
+            _ => Brushes.LightGray
+        };
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Left)
+            {
+                _isDrawing = true;
+                _startPoint = e.Location;
+                _currentPoint = e.Location;
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (_isDrawing)
+            {
+                _currentPoint = e.Location;
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (_isDrawing && e.Button == MouseButtons.Left)
+            {
+                _isDrawing = false;
+                _shapes.Add(CreateShape(_currentType, _startPoint, e.Location));
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            using (Pen pen = new Pen(Color.Black, 1))
+            {
+                foreach (var shape in _shapes)
+                {
+                    shape.Draw(g, pen, GetFillBrush(shape));
+                }
+
+                if (_isDrawing)
+                {
+                    using (Pen rubberPen = new Pen(Color.Gray, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash })
+                    using (Brush rubberBrush = new SolidBrush(Color.FromArgb(100, Color.LightGray)))
+                    {
+                        Shape rubberShape = CreateShape(_currentType, _startPoint, _currentPoint);
+                        rubberShape.Draw(g, rubberPen, rubberBrush);
+                    }
+                }
+            }
         }
     }
 }
